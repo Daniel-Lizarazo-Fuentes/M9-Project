5d369dfee561aecb7146bf6d8e9f39c2cd7a2644
Daniel-Lizarazo-Fuentes <daniel.lizarazo.fuentes@antipy.com>
2022-09-27 13:10:57 +0200

    removed prints

--- battery/batteryco2.py
+++ battery/batteryco2.py
@@ -59,11 +59,6 @@ def batteryco2(battery, prices, co2, profile, lossfree):
                     # Check if the sum of changes requested until the next battery charge are smaller than soc.
                     if (soc + (sum(tempProfile[i:indexNextRequestedBatteryCharge]) / 1000) < battery.batminsoc):
 
-                        # print(soc)
-                        # print(profile[i])
-                        # print(profile[i:indexNextRequestedBatteryCharge])
-                        # print(co2[i:indexNextRequestedBatteryCharge])
-
                         # If Co2 below average in the interval
                         if (co2[i] < (sum(co2[i:indexNextRequestedBatteryCharge]) / len(
                                 co2[i:indexNextRequestedBatteryCharge]))):
@@ -94,36 +89,6 @@ def batteryco2(battery, prices, co2, profile, lossfree):
             soc += change / 1000
             socValues.append(soc)
 
-        # Adjust array based on usage
-        # soc = copy.deepcopy(battery.batsoc)
-        # for i in range(0, len(profile)):
-        #     change = planning[i]
-        #
-        #     # User discharges and battery discharges
-        #     if (planning[i] < 0 and profile[i] < 0):
-        #         pass
-        #     # User discharges and battery charges
-        #     elif (planning[i] > 0 and profile[i] < 0):
-        #         pass
-        #     # User charges and battery charges
-        #     elif (planning[i] > 0 and profile[i] > 0):
-        #         pass
-        #     # User charges and battery discharges
-        #     elif (planning[i] < 0 and profile[i] > 0):
-        #         pass
-        #
-        #     # haircuts the change
-        #     change = cutCurrent(soc, change)
-        #     planning[i] = change
-        #
-        #     soc += change / 1000
-        #     socValues.append(soc)
-        #
-        #     # print('\n')
-        #     # print(planning[i])
-        #     # print(soc)
-
-
     else:
         # Lossy implementation
 
@@ -134,12 +99,4 @@ def batteryco2(battery, prices, co2, profile, lossfree):
     # Finally, the resulting planning for the device must be returned
     # This is also a list, with each value representing the power consumption (average) during an interval in Watts
     # The length of this list must be equal to the input vectprs (i.e., prices, co2 and profile)
-
-    # print('\n')
-    # print(co2)
-    # print(profile)
-    # print(planning)
-    # print(socValues)
-    # print('\n')
-
     return planning
